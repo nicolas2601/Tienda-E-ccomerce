0fb771019d78530f5d69830ec9af980d8c530a71
Kacper <kacpergumieniuk@gmail.com>
2026-01-26 11:11:35 +0100

    Add ignoreBuildErrors to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -49,7 +49,10 @@ const nextConfig: NextConfig = {
         hostname: '**'
       }
     ]
+  },
+  typescript: {
+    ignoreBuildErrors: true
   }
 };
 
-export default nextConfig
+export default nextConfig;
